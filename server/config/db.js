e6392f23b625a7224870c6dde2e9e6dce39a7c8d
Bunmi Alao <alaobunmi93@gmail.com>
2023-07-01 14:24:35 +0400

    fix

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,11 @@ const redis = require('redis');
 
 // Redis configuration
 const redisClient = redis.createClient({
-    host: 'localhost',
-    port: 6379,
+    socket: {
+        host: 'redis-15522.c91.us-east-1-3.ec2.cloud.redislabs.com',
+        port: 15522
+    }
 });
-
 redisClient.on('connect', () => {
     console.log('Connected to Redis server');
 });
