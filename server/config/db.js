16d9e7dad75c7e8070315e2c9549fc3722679fdd
Bunmi Alao <alaobunmi93@gmail.com>
2023-07-01 16:42:38 +0400

    db port fixes

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,14 @@
 const redis = require('redis');
+const db_port = process.env.DB_PORT || 15522;
 
 // Redis configuration
 const redisClient = redis.createClient({
     host: 'redis-15522.c91.us-east-1-3.ec2.cloud.redislabs.com',
-    port: 15522
+    port: db_port
 });
 
 redisClient.on('connect', () => {
-
-    console.log(`Connected to Redis server`);
+    console.log('Connected to Redis server');
 });
 
 redisClient.on('error', (error) => {
--- server/dist/config/db.js
+++ server/dist/config/db.js
@@ -1,9 +1,10 @@
 const redis = require('redis');
+const db_port = process.env.DB_PORT || 15522;
 
 // Redis configuration
 const redisClient = redis.createClient({
     host: 'redis-15522.c91.us-east-1-3.ec2.cloud.redislabs.com',
-    port: 15522
+    port: db_port
 });
 
 redisClient.on('connect', () => {
